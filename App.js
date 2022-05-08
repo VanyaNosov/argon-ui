f2b5593671959fc147bba050f2bd655cabe53a53
vanyanosov <nosov582125@gmail.com>
2022-05-08 14:42:54 +0300

    drawer-navigation

--- App.js
+++ App.js
@@ -1,42 +1,18 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { SignUp } from './src/components/Sing-up';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from './src/screens/Home';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import DrawerContent from './src/DrawerContent'
+
+import { DrawerNavigate } from './src/navigation/DrawerNavigation';
 
 const Stack = createNativeStackNavigator()
-const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} />
-      </Drawer.Navigator>
+      <DrawerNavigate />
     </NavigationContainer>
-    // <NavigationContainer>
-    //   <Stack.Navigator
-    //     screenOptions={{
-    //       headerStyle: {
-    //         backgroundColor: '#f4511e',
-    //       },
-    //       headerTintColor: '#fff',
-    //       headerTitleStyle: {
-    //         fontWeight: 'bold',
-    //       },
-    //       headerRight: () => (
-    //         <Button
-    //           onPress={() => alert('This is a button!')}
-    //           title="Info"
-    //           color="red"
-    //         />
-    //       ),
-    //     }}
-    //   >
-    //     <Stack.Screen name="Home" component={Home} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-  );
+  )
 }
---
+++ src/navigation/DrawerNavigation.js
@@ -0,0 +1,40 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem,
+} from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+
+export function DrawerNavigate() {
+  return (
+    <Drawer.Navigator initialRouteName="Home" useLegacyImplementation>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  );
+}
